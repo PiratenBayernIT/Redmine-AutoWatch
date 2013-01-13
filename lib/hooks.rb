3223a4e03ba3365518cbd3efeaff9b2f72fe7cad
Andreas Litt <andreas.litt@gmail.com>
2013-01-13 21:23:56 +0100

    Hook for issue controller

--- lib/hooks.rb
+++ lib/hooks.rb
@@ -1,5 +1,18 @@
 class Hooks < Redmine::Hook::ViewListener
-  def view_projects_form(context={} )
-    return content_tag("p", "Custom content added to the left")
+  def controller_issues_new_before_save(context={} )
+    dest_role = Setting.plugin_autowatch['role_id'].to_i
+    issue = context[:issue]
+    project = Project.find(issue.project_id) # Get Project
+    project.members.each do |member| # Get all members from Project and loop through them
+      member.roles.each do |role|
+        if role.id == dest_role
+          # Member is in autowatch role
+          user = member.user
+          if !(issue.watched_by? user)
+            issue.add_watcher(user)
+          end
+        end
+      end
+    end
   end
 end
